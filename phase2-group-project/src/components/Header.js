d660be424212fce25f5243177b53efe3a94e54e9
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-03-07 16:07:25 -0500

    nav-bar-style

--- phase2-group-project/src/components/Header.js
+++ phase2-group-project/src/components/Header.js
@@ -4,13 +4,15 @@ import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
 
 function Header() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Flation Library (Header)</h1>
-      <NavLink to="/" exact>Home</NavLink>
-      <NavLink to="/login" exact>Login</NavLink>
-      <NavLink to="/mybooks" exact>My Books</NavLink>
-      <NavLink to="/about" exact>About Us</NavLink>
-    </div>
+  <div className="">
+    <h1 className="w-full text-5xl font-mono font-medium bg-red-700 text-center">Flation Library</h1>
+     <ul className="uppercase font-semibold">
+      <li className='p-4 border-b border-gray-400'> <NavLink to="/" exact>Home</NavLink></li>
+      <li className='p-4 border-b border-gray-400'> <NavLink to="/login" exact>Login</NavLink></li>
+      <li className='p-4 border-b border-gray-400'> <NavLink to="/mybooks" exact>My Books</NavLink></li>
+      <li className='p-4 border-b border-gray-400'> <NavLink to="/about" exact>About Us</NavLink></li>
+    </ul>
+  </div>
   );
 }
 
