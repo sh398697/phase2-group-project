9f08025eceb69ee4f7d6b3d8f873954e3017e61b
Scott <scotthenry1@gmail.com>
2023-03-09 15:44:20 -0500

    adjusting header spacing

--- phase2-group-project/src/components/Header.js
+++ phase2-group-project/src/components/Header.js
@@ -7,8 +7,8 @@ function Header({currentUser}) {
   return (
   <div className="w-screen h-[120px] z-10 bg-slate-600 justify-center items-center">
       <div className="justify-center items-center">
-        <div><h1 className="text-3xl py-4 font-bold sm:text-4xl text-white mx-auto p-3"><NavLink to="/" exact>Flation Library</NavLink></h1></div>
-        <div>{((currentUser === "") ? (<NavLink to="/login" exact><span className="text-white text-base pt-4 p-3"></span></NavLink>) : (<span className="text-white text-base pt-4 p-3">Welcome, {currentUser}</span>))}</div>
+        <div><h1 className="text-3xl font-bold sm:text-4xl text-white mx-auto pt-6"><NavLink to="/" exact>Flation Library</NavLink></h1></div>
+        <div>{((currentUser === "") ? (<NavLink to="/login" exact><span className="text-white text-base pt-4 p-3"></span></NavLink>) : (<span className="text-white text-base pl-3">Welcome, {currentUser}</span>))}</div>
 
         <ul className="flex flex-initial h-[120px] z-10 bg-slate-600 items-center justify-center mx-auto">
           <NavLink to="/" exact className="flex-initial w-48"><li>Home</li></NavLink>
