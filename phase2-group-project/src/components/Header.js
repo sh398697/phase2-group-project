596f31dbbf9b24f7d376a4bffb643de53842552a
Scott <scotthenry1@gmail.com>
2023-03-09 11:29:30 -0500

    navbar styling tweaks

--- phase2-group-project/src/components/Header.js
+++ phase2-group-project/src/components/Header.js
@@ -9,7 +9,7 @@ function Header({currentUser}) {
       <div className="justify-center items-center">
         <NavLink to="/" exact><h1 className="text-3xl py-4 font-bold sm:text-4xl text-white mx-auto p-3">Flation Library</h1></NavLink>
         {((currentUser === "") ? (<span className="float-right"></span>) : (<span className="float-right font-bold mx-7">Welcome, {currentUser}</span>))}
-        <ul className="hidden md:flex justify-center text-white">
+        <ul className="container flex w-screen h-[120px] z-10 bg-slate-600 items-center justify-center mx-auto">
           <li> <NavLink to="/" exact>Home</NavLink></li>
           <li> <NavLink to="/login" exact>My Account</NavLink></li>
           <li> <NavLink to="/mybooks" exact>My Books</NavLink></li>
