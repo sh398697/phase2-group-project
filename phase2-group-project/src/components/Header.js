b07413eeef2422defe617557f2f0e805d1ca4f16
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-03-09 15:57:58 -0500

    ul height

--- phase2-group-project/src/components/Header.js
+++ phase2-group-project/src/components/Header.js
@@ -10,7 +10,7 @@ function Header({currentUser}) {
         <div><h1 className="text-3xl font-bold sm:text-4xl text-white mx-auto pt-6"><NavLink to="/" exact>Flation Library</NavLink></h1></div>
         <div>{((currentUser === "") ? (<NavLink to="/login" exact><span className="text-white text-base pt-4 p-3"></span></NavLink>) : (<span className="text-white text-base pl-3">Welcome, {currentUser}</span>))}</div>
 
-        <ul className="flex flex-initial h-[120px] z-10 bg-slate-600 items-center justify-center mx-auto">
+        <ul className="flex flex-initial h-[95px] z-10 bg-slate-600 items-center justify-center mx-auto">
           <NavLink to="/" exact className="flex-initial w-48"><li>Home</li></NavLink>
           <NavLink to="/login" exact className="flex-initial w-48"><li>My Account</li></NavLink>
           <NavLink to="/mybooks" exact className="flex-initial w-48"><li>My Books</li></NavLink>
