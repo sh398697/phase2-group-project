8a4ca511965abf500bb47b7a7664b1bd6d03668a
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-03-08 22:21:09 -0500

    navBar style plus about us

--- phase2-group-project/src/components/Header.js
+++ phase2-group-project/src/components/Header.js
@@ -5,18 +5,18 @@ import { NavLink } from "react-router-dom";
 function Header({currentUser}) {
 
   return (
-  <div className="">
-    <div>
-      <span><h1 className="w-full text-5xl font-mono font-medium bg-red-800 text-center">Flation Library</h1></span>
-      {((currentUser === "") ? (<span className="float-right"></span>) : (<span className="float-right font-bold mx-7">Welcome, {currentUser}</span>))}
-    </div>
-     <ul className="uppercase font-semibold">
-      <li className='p-4 border-b border-gray-400'> <NavLink to="/" exact>Home</NavLink></li>
-      <li className='p-4 border-b border-gray-400'> <NavLink to="/login" exact>Login</NavLink></li>
-      <li className='p-4 border-b border-gray-400'> <NavLink to="/mybooks" exact>My Books</NavLink></li>
-      <li className='p-4 border-b border-gray-400'> <NavLink to="/newbook" exact>New Book Form</NavLink></li>
-      <li className='p-4 border-b border-gray-400'> <NavLink to="/about" exact>About Us</NavLink></li>
-    </ul>
+  <div className="w-screen h-[100px] z-10 bg-slate-600">
+      <div className="">
+         <h1 className="text-3xl font-bold sm:text-4xl text-white mx-auto p-3">Flation Library</h1>
+        {((currentUser === "") ? (<span className="float-right"></span>) : (<span className="float-right font-bold mx-7">Welcome, {currentUser}</span>))}
+        <ul className="hidden md:flex justify-end text-white">
+          <li> <NavLink to="/" exact>Home</NavLink></li>
+          <li> <NavLink to="/login" exact>Login</NavLink></li>
+          <li> <NavLink to="/mybooks" exact>My Books</NavLink></li>
+          <li> <NavLink to="/newbook" exact>New Book Form</NavLink></li>
+          <li> <NavLink to="/about" exact>About Us</NavLink></li>
+       </ul>
+      </div>
   </div>
   );
 }
