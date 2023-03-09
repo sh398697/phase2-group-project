2c905abde21d89caf963b5a7aba2cda8defbeb0f
Scott <scotthenry1@gmail.com>
2023-03-09 09:28:43 -0500

    fix to My Books page to properly show no books checked out message

--- phase2-group-project/src/components/MyBooks.js
+++ phase2-group-project/src/components/MyBooks.js
@@ -12,16 +12,7 @@ function MyBooks({books, currentUser, returnBook, removeBookFromState, checkOutB
     })
 
     const bookList = myBooks.map(book => {
-        return (<BookCard key={book.id} book={book} removeBookFromState={removeBookFromState} currentUser={currentUser} checkOutBook={checkOutBook} isMyBooks={true} handleReturnBookClick={handleReturnBookClick}/>
-            
-            
-            /*
-            <div key={book.id}>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{book.title}</span>
-                <button onClick={() => handleReturnBookClick(book)}>Return Book</button>
-            </div>
-            */
-        )
+        return (<BookCard key={book.id} book={book} removeBookFromState={removeBookFromState} currentUser={currentUser} checkOutBook={checkOutBook} isMyBooks={true} handleReturnBookClick={handleReturnBookClick}/>)
     })
 
     function handleReturnBookClick(book) {
@@ -29,9 +20,9 @@ function MyBooks({books, currentUser, returnBook, removeBookFromState, checkOutB
     }
 
     return (
-    <div className="flex justify-center container mx-auto bg-gray-200 rounded-xl border p-8 m-10">
-        <h1 className="font-bold">My Books</h1>
-        { (myBooks) ? (
+    <div className="justify-center container mx-auto bg-gray-200 rounded-xl border p-8 m-10">
+        <div><h1 className="font-bold">My Books</h1></div>
+        { (myBooks.length > 0) ? (
             <div>
                 <div>Your checked out books:</div>
                 <div className="grid md:grid-cols-5 lg:grid-cols-5 gap-4 lg:gap-1">{bookList}</div>
